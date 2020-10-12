f4cd1f3b933e23b4946b107162d576c8fa737a54
Frozes <gunzb92@gmail.com>
2020-10-12 22:35:17 +0700

    Move head content from layout to include

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -1,18 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Profile') }}</title>
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-        @livewireStyles
-
-        <!-- Scripts -->
-        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.0/dist/alpine.js" defer></script>
+        @include('include.head')
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
