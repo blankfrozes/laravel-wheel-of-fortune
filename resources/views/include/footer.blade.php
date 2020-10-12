5ea70d32b9f59d9107185644d3ed01651cabcd2a
Frozes <gunzb92@gmail.com>
2020-10-12 22:48:09 +0700

    :art: Add example file

---
+++ resources/views/include/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="w-full px-20 py-6 text-lg text-center text-white bg-blue">
+        Footer
+    </div>
+</footer>
---
+++ resources/views/pages/profile.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.home')
+
+@section('content')
+<div class="w-full h-90vh flex justify-start item-center">
+
+    <div class="container">
+        This is Body
+    </div>
+
+</div>
+@endsection
